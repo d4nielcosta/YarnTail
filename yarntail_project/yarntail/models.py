df580c3211f24a8dd47732cdf6088266276d80cb
2082657p <2082657p@student.gla.ac.uk>
2015-03-21 03:19:48 +0000

    pattern input

--- yarntail_project/yarntail/models.py
+++ yarntail_project/yarntail/models.py
@@ -1,7 +1,36 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.template.defaultfilters import slugify
+import ast
 
+class ListField(models.TextField):
+    __metaclass__ = models.SubfieldBase
+    description = "Stores a python list"
+
+    def __init__(self, *args, **kwargs):
+        super(ListField, self).__init__(*args, **kwargs)
+
+    def to_python(self, value):
+        if not value:
+            value = []
+
+        if isinstance(value, list):
+            return value
+
+        return ast.literal_eval(value)
+
+    def get_prep_value(self, value):
+        if value is None:
+            return value
+
+        return unicode(value)
+
+    def value_to_string(self, obj):
+        value = self._get_val_from_obj(obj)
+        return self.get_db_prep_value(value)
+
+class ListModel(models.Model):
+    test_list = ListField()
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, related_name='user_profile')
@@ -30,6 +59,8 @@ class Pattern(models.Model):
     description = models.CharField(max_length=3000)
     creation_date = models.DateTimeField(auto_now_add=True)
     slug = models.SlugField(unique=True)
+    design = ListField()
+    difficulty = models.CharField(default = "Easy", max_length=10)
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
