f42db6d40d3fb42b31fa412ddcc32d0cf0136824
DragonCatExcelsior <degiojake@gmail.com>
2015-03-22 16:07:22 +0000

    Comment test done.

--- yarntail_project/test.py
+++ yarntail_project/test.py
@@ -60,3 +60,32 @@ class PatternTestCase(TestCase):
         description = getattr(pattern, 'description')
         self.assertEqual(str(description), "A pair of shorts that won't break when you turn into a massive green "
                                            "rage monster. HULK SMASH!")
+
+class CommentTestCase(TestCase):
+
+    def setUp(self):
+        user1 = User.objects.create_user("Hulk", "gammaradiationisfun@greenman.com", "password")
+        UserProfile.objects.create(user=user1, first_name="Bruce", last_name="Banner", date_of_birth=datetime.date(1962, 5, 1))
+        pattern = Pattern.objects.create(title="Incredible Shorts", user=user1, difficulty="Easy",
+                               description="A pair of shorts that won't break when you turn into a massive green "
+                                           "rage monster. HULK SMASH!")
+
+        user2 = User.objects.create_user("Abomination", "abonimationrox@abom.com", "abomIsGr8")
+        UserProfile.objects.create(user=user2, first_name="Emil", last_name="Blonsky", date_of_birth=datetime.date(1964, 4, 1))
+        Comment.objects.create(user=user2, pattern=pattern,
+                               comment_string="Shorts ripped on abomination...Abomination stronger than Hulk!")
+
+
+    def test_Comment_User(self):
+        user = getattr(Comment.objects.get(user=User.objects.get(username="Abomination")), 'user')
+        self.assertEqual(str(user), "Abomination")
+
+    def test_Comment_Pattern(self):
+        comment = Comment.objects.get(user=User.objects.get(username="Abomination"))
+        pattern = getattr(comment, 'pattern')
+        self.assertEqual(str(pattern), "Incredible Shorts")
+
+    def test_Comment_String(self):
+        comment = Comment.objects.get(user=User.objects.get(username="Abomination"))
+        data = getattr(comment, 'comment_string')
+        self.assertEqual(str(data), "Shorts ripped on abomination...Abomination stronger than Hulk!")
