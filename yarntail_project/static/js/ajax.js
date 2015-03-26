3ef371f0d434e61165cc3ed9e06e84237fde6207
Joshua Marsh <2155744M@student.gla.ac.uk>
2015-03-26 18:56:59 +0000

    autocomplete search drop down working

--- yarntail_project/static/js/ajax.js
+++ yarntail_project/static/js/ajax.js
@@ -11,28 +11,20 @@ $(function () {
             success: searchSuccess,
             dataType: 'json'
         });
-        //alert(data);
     });
-
-    //$('#search-button').click(function () {//search button works without this, remove after a day or two
-    //    query = $('#search').val();
-    //    spacelessQuery = query.split(" ").join("+");
-    //    link = "/yarntail/search_results/" + spacelessQuery;
-    //    window.location.replace(link);
-    //});
 
 });
 
 function searchSuccess(data, textStatus, jqXHR) {
-    $('#results').empty();
-    console.log("asfghj");
-    console.log(data);
+    $('#searchlist').empty();
 
+    $('#searchlist').append('<option> -----Patterns----- </option>');
     for (pattern in data['patterns']) {
-        $('#results').append('<li><a href="' + data['patterns'][pattern]['url'] + '">' + data['patterns'][pattern]['title'] + '</a></li>');
+        $('#searchlist').append('<option><a href="' + data['patterns'][pattern]['url'] + '">' + data['patterns'][pattern]['title'] + '</a></option>');
     }
+    $('#searchlist').append("<option> -----Users----- </option>");
     for (user in data['users']) {
-    $('#results').append('<li><a href="' + data['users'][user]['url'] + '">' + data['users'][user]['name'] + '</a></li>');
+    $('#searchlist').append('<option><a href="' + data['users'][user]['url'] + '">' + data['users'][user]['name'] + '</a></option>');
     }
 }
 
